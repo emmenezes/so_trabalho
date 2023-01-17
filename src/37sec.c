a2167ec098eee20efdde9a9797a793f6da8a5ea5
Emme Menezes <menezes.ecd@gmail.com>
2023-01-17 18:34:52 -0300

    feat: adiciona programas para execução

---
+++ src/37sec.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    long int i;
+    for (i = 0; i < 20000000000; i++); // 37s
+    // for (i = 0; i < 8000000000; i++); // 14s
+
+    return 0;
+}
