1a8652dae62c9fa37ea9845fc5c779bea423c3e5
Emme Menezes <menezes.ecd@gmail.com>
2023-01-16 23:53:21 -0300

    feat: complementa cancela_proc

--- src/cancela_proc.c
+++ src/cancela_proc.c
@@ -1,19 +1,55 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) 
+#define IPC_ID 2353
+
+struct msg
 {
-   if(argc < 2)
-   {
-    printf("Erro, qtd agurmentos.");
-    exit(5);
-   }
-   kill(atoi(argv[1]), SIGKILL);
+    int id;
+    char file_name[30];
+};
+
+int main(int argc, char *argv[])
+{
+    int idfila, LEN = 10;
+    long arg = 0;
+    char line[LEN];
+    char *p;
+    struct msg msg_env;
+
+    if (argc == 2)
+    {
+        arg = strtol(argv[1], &p, 10);
+        if (*p != '\0' || errno != 0)
+        {
+            printf("Argumento invalido, insera apenas o numero do processo que deseja encerrar\n");
+            return 1; 
+        }
+        msg_env.id = arg;
+        strcpy(msg_env.file_name, "");
+    }
+    else
+    {
+        printf("Quantidade de argumentos invalida, insira apenas o número do processo que deseja encerrar\n");
+        return 1;
+    }
+
+    if ((idfila = msgget(IPC_ID, 0x124)) < 0)
+    {
+        printf("erro na obtenção da fila da fila\n");
+    }
+    msgsnd(idfila, &msg_env, sizeof(msg_env), 0);
+
+    FILE *cmd = popen("pidof ex2", "r");
+    fgets(line, LEN, cmd);
+    pid_t pid = strtoul(line, NULL, 10);
+    kill(pid, SIGUSR2);
+    pclose(cmd);
 }
